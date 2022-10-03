9121369597d79e308ac7e54d173ca89a4daeafc2
Aleex55 <94048476+Aleex55@users.noreply.github.com>
2022-10-03 12:47:12 +0200

    imagedice

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.diceroll
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -11,17 +13,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val elTexto = findViewById<TextView>(R.id.textView) as TextView;
-        val elBoton = findViewById<Button>(R.id.button) as Button;
+        val laImagen = findViewById<ImageView>(R.id.imageView) as ImageView
+        val elBoton = findViewById<Button>(R.id.button) as Button
 
         elBoton.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Rolling the dice!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this@MainActivity, "Rolling the dice!", Toast.LENGTH_SHORT).show()
 
-            elTexto.text = roll().toString();
 
         }
     }
-    fun roll(): Int {
-        return (1..6).random();
+    fun roll(){
+        val laImagen = findViewById<ImageView>(R.id.imageView) as ImageView;
+        val elRandom =  (1..6).random();
+
+        when (elRandom) {
+            1 -> laImagen.setImageResource(R.drawable.dice_1);
+            2 -> laImagen.setImageResource(R.drawable.dice_2);
+            3 -> laImagen.setImageResource(R.drawable.dice_3);
+            4 -> laImagen.setImageResource(R.drawable.dice_4);
+            5 -> laImagen.setImageResource(R.drawable.dice_4);
+            else -> { // Note the block
+                laImagen.setImageResource(R.drawable.dice_6);
+            }
+        }
+
     }
 }
